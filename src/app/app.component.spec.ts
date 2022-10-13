9e4d4cc079106e2b52701501567147dbe7d8af1c
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-13 17:29:38 +0530

    new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'courseapp'`, () => {
+  it(`should have as title 'courseapp-clone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('courseapp');
+    expect(app.title).toEqual('courseapp-clone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('courseapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('courseapp-clone app is running!');
   });
 });
